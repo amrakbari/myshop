1b4ce6d0b659aad8806f309d0f463da9bb8c3d93
Amirreza Akbari <amrakbaridev@gmail.com>
2023-12-12 23:19:26 +0330

    feat: added new models (Address, Store)

--- myshop/store/models.py
+++ myshop/store/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
 from myshop.common.models import BaseModel
+from myshop.users.models import BaseUser
+
+
+class Address(BaseModel):
+    user = models.ForeignKey(BaseUser, on_delete=models.CASCADE)
+    address = models.TextField()
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255)
+    is_default = models.BooleanField(default=False)
+
+
+class Store(BaseModel):
+    user = models.ForeignKey(BaseUser, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
 
 
 # Create your models here.
