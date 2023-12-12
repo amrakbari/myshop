4c0e06bf061df699bc28b81ed6661287b3d1dae6
Amirreza Akbari <amrakbaridev@gmail.com>
2023-12-12 23:28:44 +0330

    refactor: moved all APIs to single file for better accessibility

--- myshop/store/urls.py
+++ myshop/store/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
-from myshop.store.apis.products import ProductApi
-from myshop.store.apis.product_image import ProductImageApi
+from myshop.store.apis import ProductApi
+from myshop.store.apis import ProductImageApi
 
 urlpatterns = [
     path('products/', ProductApi.as_view()),
